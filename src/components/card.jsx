0c36e483b7a7d0836986e7417c969156f97aaf38
filipe <filipemard@gmail.com>
2022-04-01 16:51:21 -0300

    changes of css

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,20 +1,26 @@
-import {FcSimCardChip} from "react-icons/fc";
+import { FcSimCardChip } from "react-icons/fc";
 
-export default function card({numbCard, nameCard,secretNcard}) {
+export default function card({ numbCard, nameCard, secretNcard }) {
   return (
     <section className="Card">
-        <div>
-            <FcSimCardChip/>
+      <div className="conte">
+        <div className="icons">
+          <div className="chip">
+            <FcSimCardChip />
+          </div>
+          <img src="" alt="bandeira" />
         </div>
-        <img src="" alt="bandeira"/>
-        <h3>5454 5454 5454 5454</h3>
-        <h4>Filipe Marques</h4>
-        <p>1234</p>
-        {/* 
+        <h3 className="numero">5454 5454 5454 5454</h3>
+        <div className="content">
+          <h4 className="nome">Filipe Marques</h4>
+          <p>1234</p>
+        </div>
+      </div>
+      {/* 
         <h3>{numbCard}</h3>
         <h4>{nameCard}</h4>
         <p>{secretNcard}</p>
         */}
     </section>
-  )
+  );
 }
